2fa3e7bcda8a0744a52c56c6b496e7d7d377a1f5
ltgan1121 <ltgan1121@qq.com>
2021-07-27 17:09:12 +0800

    demo for vue3 & antd

---
+++ hello-v3/src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-select
+    v-model:value="value1"
+    style="width: 120px"
+    @focus="focus"
+    ref="select"
+    @change="handleChange"
+  >
+    <a-select-option value="jack">Jack</a-select-option>
+    <a-select-option value="lucy">Lucy</a-select-option>
+    <a-select-option value="disabled" disabled>Disabled</a-select-option>
+    <a-select-option value="Yiminghe">yiminghe</a-select-option>
+  </a-select>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    const focus = () => {
+      console.log('focus');
+    };
+
+    const handleChange = value => {
+      console.log(`selected ${value}`);
+    };
+
+    return {
+      focus,
+      handleChange,
+      value1: ref('lucy'),
+    };
+  },
+});
+</script>
